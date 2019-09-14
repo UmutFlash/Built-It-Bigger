94fd63c6a009aeee63731be74420b499c8ecbe26
Ümüt Yildirim <umut.yildirim@gmx.net>
2019-09-14 15:51:03 +0200

    Step 3: Setup GCE

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -44,11 +46,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, MyJoke.getJoke(), Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, JokeActivity.class);
-        String joke = MyJoke.getJoke();
-        intent.putExtra(JokeActivity.JOKE, joke);
-        startActivity(intent);
+        new EndpointsAsyncTask(this).execute();
     }
 }
