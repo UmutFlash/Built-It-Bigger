095cec8cd9dc9eb8dfefb1d89fe7ff3dacffa6dc
Ümüt Yildirim <umut.yildirim@gmx.net>
2019-09-19 15:06:17 +0200

    Step 5: Add a Paid Flavor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
@@ -8,20 +9,22 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
+
     @Test
-    public void testJokes() throws Exception {
+    public void testDoInBackground() throws Exception{
         try {
             MainActivity mainActivity = new MainActivity();
-            EndpointsAsyncTask endpointsTask = new EndpointsAsyncTask(mainActivity);
-            endpointsTask.execute();
-            String result = endpointsTask.get(10, TimeUnit.SECONDS);
+            EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(mainActivity);
+            endpointsAsyncTask.execute();
+            String result = endpointsAsyncTask.get(30, TimeUnit.SECONDS);
 
             assertNotNull(result);
-        } catch (Exception e) {
+        } catch (Exception e){
             Log.e("EndpointsAsyncTaskTest", e.getMessage());
         }
     }
